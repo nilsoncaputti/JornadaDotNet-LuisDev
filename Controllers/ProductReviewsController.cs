1ff4d95ab75683388a81b321ee9b0dca78660144
nilsoncaputti <nilson_caputti@hotmail.com>
2021-09-19 11:31:25 -0300

    Primeiro Commit

---
+++ Controllers/ProductReviewsController.cs
@@ -0,0 +1,28 @@
+//using System.Threading.Tasks;
+//using AutoMapper;
+//using DevReviews.API.Entities;
+using DevReviews.API.Models;
+//using DevReviews.API.Persistence.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevReviews.API.Controllers
+{
+    [ApiController]
+    [Route("api/products/{productId}/productreviews")]
+    public class ProductReviewsController : ControllerBase {
+
+        // GET api/products/1/productreviews/5
+        [HttpGet("{id}")]
+        public IActionResult GetById(int productId, int id) {
+            // Se não existir com o ID especificado, retornar NotFound()            
+            return Ok();
+        }
+
+        // POST api/products/1/productreviews
+        [HttpPost]
+        public IActionResult Post(int productId, AddProductReviewInputModel model) {
+            // Se estiver com dados inválidos, retornar BadRequest()            
+            return CreatedAtAction(nameof(GetById), new { id = 1, productId = 2 }, model);
+        }
+    }
+ }
